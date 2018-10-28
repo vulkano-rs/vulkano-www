1b8910028e8ca3bc61b85e10dd6a309521a48a0d
Rukai <rubickent@gmail.com>
2018-10-28 18:44:33 +1100

    Move shaders back

--- examples/guide-compute-operations.rs
+++ examples/guide-compute-operations.rs
@@ -51,6 +51,26 @@ fn main() {
     let data_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
                                                     data_iter).expect("failed to create buffer");
 
+    // Compute pipelines
+    mod cs {
+        vulkano_shaders::shader!{
+            ty: "compute",
+            src: "
+#version 450
+
+layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+layout(set = 0, binding = 0) buffer Data {
+    uint data[];
+} buf;
+
+void main() {
+    uint idx = gl_GlobalInvocationID.x;
+    buf.data[idx] *= 12;
+}"
+        }
+    }
+
     let shader = cs::Shader::load(device.clone())
         .expect("failed to create shader module");
     let compute_pipeline = Arc::new(ComputePipeline::new(device.clone(), &shader.main_entry_point(), &())
@@ -77,24 +97,4 @@ fn main() {
     }
 
     println!("Everything succeeded!");
-}
-
-
-mod cs {
-    vulkano_shaders::shader!{
-        ty: "compute",
-        src: "
-#version 450
-
-layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
-
-layout(set = 0, binding = 0) buffer Data {
-    uint data[];
-} buf;
-
-void main() {
-    uint idx = gl_GlobalInvocationID.x;
-    buf.data[idx] *= 12;
-}"
-    }
 }
--- examples/guide-triangle.rs
+++ examples/guide-triangle.rs
@@ -93,6 +93,34 @@ fn main() {
         .add(image.clone()).unwrap()
         .build().unwrap());
 
+    mod vs {
+        vulkano_shaders::shader!{
+            ty: "vertex",
+            src: "
+#version 450
+
+layout(location = 0) in vec2 position;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+}"
+        }
+    }
+
+    mod fs {
+        vulkano_shaders::shader!{
+            ty: "fragment",
+            src: "
+#version 450
+
+layout(location = 0) out vec4 f_color;
+
+void main() {
+    f_color = vec4(1.0, 0.0, 0.0, 1.0);
+}"
+        }
+    }
+
     let vs = vs::Shader::load(device.clone()).expect("failed to create shader module");
     let fs = fs::Shader::load(device.clone()).expect("failed to create shader module");
     
@@ -139,30 +167,3 @@ fn main() {
     image.save("image.png").unwrap();
 }
 
-mod vs {
-    vulkano_shaders::shader!{
-        ty: "vertex",
-        src: "
-#version 450
-
-layout(location = 0) in vec2 position;
-
-void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
-}"
-    }
-}
-
-mod fs {
-    vulkano_shaders::shader!{
-        ty: "fragment",
-        src: "
-#version 450
-
-layout(location = 0) out vec4 f_color;
-
-void main() {
-    f_color = vec4(1.0, 0.0, 0.0, 1.0);
-}"
-    }
-}
